222bca2beca696982e84062b20369224ed1bf41d
Nabil Fares <nabilfares@Nabils-MacBook-Pro.local>
2019-01-23 18:25:43 +0300

    Nabil fatima S

--- restaurants/models.py
+++ restaurants/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Restaurant(models.Model):
+	name = models.CharField(max_length=100)
+	description = models.TextField()
+	opening_time = models.TimeField()
+	closing_time = models.TimeField()
+
+	def __str__(self):
+		return ("%s - %s - %s - %s")  % (self.name,self.description,self.opening_time,self.closing_time)
